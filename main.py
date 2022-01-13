42dfecd24d773687bf6cbbac3e12cfdfef9461cc
Trushangpatel-13 <ptrushang@gmail.com>
2022-01-13 10:47:39 +0530

    UI

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import dash
-
-from dash import html
-from dash import dcc
+import dash_core_components as dcc
+import dash_html_components as html
 import dash_bootstrap_components as dbc
 import dash_auth
-
 from app_init import app
 from app_init import server
 import predictions, range_based, date_based, upload_train
@@ -21,10 +19,7 @@ colors = {
     'text': '#7FDBFF'
 }
 
-'''auth = dash_auth.BasicAuth(
-    app,
-    VALID_USERNAME_PASSWORD_PAIRS
-)'''
+
 
 app.layout = html.Div(children=[
     
@@ -34,33 +29,55 @@ app.layout = html.Div(children=[
 ])
 
 
-
+left_card = html.Div(
+    [
+        #html.H3("Problem Statement", className="card-title"),
+        html.P(
+            "Wind energy plays an increasing role in the supply of energy world-wide." 
+            "The energy output of a wind farm is highly dependent on the wind conditions present at its site." 
+            "If the output can be predicted more accurately, energy suppliers can coordinate the collaborative "
+            "production of different energy sources more efficiently to avoid costly overproduction."),
+        
+        html.P(
+            "Wind power or wind energy is the use of wind to provide the mechanical power through wind turbines"
+            "to turn electric generators and traditionally to do other work, like milling or pumping. Wind power "
+            "is a sustainable and renewable energy, and has a much smaller impact on the environment compared to burning "
+            "fossil fuels. Wind farms consist of many individual wind turbines, which are connected to the electric power "
+            "transmission network. Onshore wind is an inexpensive source of electric power, competitive with or in many places "
+            "cheaper than coal or gas plants. Onshore wind farms also have an impact on the landscape, as "
+            "typically they need to be spread over more land than other power stations and need to be built in wild "
+            "and rural areas, which can lead to industrialization of the countryside and habitat loss. Offshore "
+            "wind is steadier and stronger than on land and offshore farms have less visual impact, but construction "
+            "and maintenance costs are higher. Small onshore wind farms can feed some energy into the grid or provide "
+            "electric power to isolated off-grid locations."
+        ),
+        #dbc.Button("Read More", color="primary" , href="https://smartinternz.com/ibm-hack-challenge-2020"),
+    ]
+)
 
 right_card = html.Div(
         [
-
-            html.H3("Alerts", className="card-title"),
-            html.P(
-                "Wind Speed: 340 m/s"
-            ),
-            html.P(
-                "Wind Direction: 43 Deg"
-            ),
-            html.P(
-                "Power Output: 873498"
-            ),
-            html.H5(
-                "Status:"
-            ),
+           # html.H3("Alerts", className="card-title"),
+           # html.P(
+            #    "Wind Speed: 340 m/s"
+           # ),
+            #html.P(
+             #   "Wind Direction: 43 Deg"
+           # ),
+           # html.P(
+            #    "Power Output: 873498"
+            #),
+           # html.H5(
+            #    "Status:"
+            #),
             dbc.Button("Active", color="success"),
             html.Br(),
             html.Br(),
-
-            #dbc.Button("Detail Predictions", color="warning" , href="/show_predictions"),
+           # dbc.Button("Detail Predictions", color="warning" , href="/show_predictions"),
         ]
 )
 
-info = dbc.Row([dbc.Col(width=1), dbc.Col(right_card, width=3)])
+info = dbc.Row([dbc.Col(left_card, width=8), dbc.Col(width=1), dbc.Col(right_card, width=3)])
 
 index_page = html.Div(style={'position':'absolute'}, children=[
     
@@ -71,7 +88,7 @@ index_page = html.Div(style={'position':'absolute'}, children=[
             'object-fit': 'cover',
         }
     ),
-    html.H1(children='Windmill Power Forecasting',
+    html.H1(children='SMART PREDICTIVE MAINTENANCE AND OPERATIONS FOR WINDMILLS',
             style={
                 'color':colors['background'],
                 'textAlign':'center','position':'absolute',
@@ -79,7 +96,7 @@ index_page = html.Div(style={'position':'absolute'}, children=[
                 'left':'50%',
                 'transform':'translate(-50%, -50%)',
                 'fontSize': '3vw',
-                'textDecoration':'underline overline',
+              #  'textDecoration':'underline overline',
             }
     ),
     html.H1(children='Predicting the energy output of wind turbine based on weather condition',
@@ -95,7 +112,16 @@ index_page = html.Div(style={'position':'absolute'}, children=[
                 # 'opacity':'0.8'
             }
     ),
-
+    #html.P(dbc.Button("Learn more", color="primary", href="/show_factors"), className="lead" ,
+           # style={
+               # 'color':'black',
+               # 'textAlign':'center',
+               # 'position':'absolute',
+               # 'top':'47%',
+               # 'left':'50%',
+              #  'transform':'translate(-50%, -50%)',
+           # }
+    #),
             
     html.Br(),
     html.Br(),
@@ -171,7 +197,7 @@ navbar = dbc.NavbarSimple(
 
 
     ],
-    brand="",
+    brand="Capstone Project",
     brand_href="#",
     color="dark",
     dark=True,
